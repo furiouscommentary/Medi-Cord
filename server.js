8b141a3053f5fa251204b3fa34edf4c218004da5
Nitin Tiwari <111339364+furiouscommentary@users.noreply.github.com>
2025-03-22 00:33:06 +0530

    vercel dynamic port handling

--- server.js
+++ server.js
@@ -212,4 +212,9 @@ function checkNotAuthenticated(req, res, next){
 
 
 //PORT SETUP
-app.listen(3000,()=> console.log("server has started on port 3000"))
+
+if (process.env.NODE_ENV !== "production"){
+    module.exports = app;
+} else {
+    app.listen(3000,()=> console.log("server has started on port 3000"))
+}
